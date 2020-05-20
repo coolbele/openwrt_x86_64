b44b24b469a372d44f944c407bfc4eb3e4b28f2c
coolbele <64392071+coolbele@users.noreply.github.com>
2020-05-20 12:02:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 #=================================================
-# Description: Build OpenWrt using GitHub Actions
+# Description: DIY script
 # Lisence: MIT
-# Author: kenzo
-# https://github.com/kenzok8
+# Author: P3TERX
+# Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-cd lede/package
-git clone https://github.com/kenzok8/litte.git
-git clone https://github.com/kenzok8/small.git
-rm -rf package/lean/luci-theme-argon
-sed -i 's/LuCI Master/by kenzo/g' /usr/lib/lua/luci/version.lua
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-./scripts/feeds update -a
-./scripts/feeds install
+sed -i 's/192.168.1.1/192.168.1.240/g' package/base-files/files/bin/config_generate
+#src-git lienol https://github.com/Lienol/openwrt-package
+git clone https://github.com/vernesong/OpenClash                                 package/clash
+git clone https://github.com/rufengsuixing/luci-app-adguardhome                  package/adguardhome
+git clone https://github.com/tzxiaozhen88/koolproxyR                         package/koolproxyR
+git clone https://github.com/ElonH/Rclone-OpenWrt                 package/rclone
+git clone https://github.com/frainzy1477/luci-app-clash           package/luci-app-clash
+git clone https://github.com/jerrykuku/luci-theme-argon           package/luci-theme-argon
+# git clone https://github.com/longcat99/opdiy/tree/master/diy/luci-app-passwall                      package/luci-app-passwall
+git clone https://github.com/fw876/helloworld                       package/luci-app-ssr-plus
